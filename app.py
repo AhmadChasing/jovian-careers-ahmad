5d4d84cd3b7eb66acb369b5a767084bf34e0c05c
Suleiman Ahmad <139409257+AhmadChasing@users.noreply.github.com>
2025-09-13 12:11:20 +0000

    my first replit push

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def home():
-    return '''
-    <h1>Jovian Careers</h1>
-    <p>Welcome to the careers portal!</p>
-    <p>You can now build whatever you want here.</p>
-    '''
+def hello_world():
+    return 'Hello, jovian!'
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', debug=True)
